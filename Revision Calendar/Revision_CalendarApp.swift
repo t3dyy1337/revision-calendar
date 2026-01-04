773d7ae4aa322c947f87da29c50598abf408909c
Tadeas Juricek <juricektadeas@gmail.com>
2026-01-04 17:55:04 +0100

    Initial Commit

---
+++ Revision Calendar/Revision_CalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Revision_CalendarApp.swift
+//  Revision Calendar
+//
+//  Created by Tadeáš Juříček on 04.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct Revision_CalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Revision CalendarTests/Revision_CalendarTests.swift
@@ -0,0 +1,17 @@
+//
+//  Revision_CalendarTests.swift
+//  Revision CalendarTests
+//
+//  Created by Tadeáš Juříček on 04.01.2026.
+//
+
+import Testing
+@testable import Revision_Calendar
+
+struct Revision_CalendarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
